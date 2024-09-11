dc7e5d045e28f397b4eed6399cdc706c8e85b4bc
Denis Macedo <denisrmacedo@gmail.com>
2024-09-11 14:08:58 -0300

    criar projeto

---
+++ src/main/java/com/example/movit_backend_java/MovitBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.movit_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovitBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovitBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movit_backend_java/MovitBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movit_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovitBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

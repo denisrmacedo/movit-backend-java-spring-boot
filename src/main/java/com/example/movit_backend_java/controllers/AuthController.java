4d19fe6404426846ae161449c4f112d11552c94f
Denis Macedo <denisrmacedo@gmail.com>
2024-09-12 15:31:44 -0300

    criar RegisterRequestDTO

--- src/main/java/com/example/movit_backend_java/controllers/AuthController.java
+++ src/main/java/com/example/movit_backend_java/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.example.movit_backend_java.controllers;
 
 import com.example.movit_backend_java.domain.user.User;
 import com.example.movit_backend_java.dto.LoginRequestDTO;
+import com.example.movit_backend_java.dto.RegisterRequestDTO;
 import com.example.movit_backend_java.dto.ResponseDTO;
 import com.example.movit_backend_java.infra.security.TokenService;
 import com.example.movit_backend_java.repositories.UserRepository;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -26,6 +29,22 @@ public class AuthController {
         if(passwordEncoder.matches(body.password(), user.getPassword())) {
             String token = this.tokenService.generateToken(user);
             return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body){
+        Optional<User> user = this.repository.findByEmail(body.email());
+        if(user.isEmpty()){
+            User newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.repository.save(newUser);
+
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
         }
         return ResponseEntity.badRequest().build();
     }
